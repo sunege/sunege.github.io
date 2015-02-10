4ecf53e8b729aaceca7399d918b62ca0b3fb0659
sunege <ka24378@yahoo.co.jp>
2015-02-10 20:45:54 +0900

    velocityVerlet

--- HelloWorld/HelloWorld.js
+++ HelloWorld/HelloWorld.js
@@ -1,11 +1,172 @@
+////////////////////
+//                //
+// 1D H.O. System //
+//                //
+////////////////////
+
+//System Parameter
+var time = 0;		// time
+var dt = 0.0001; // Delta t
+var N = 8;	   // number of particle
+var L = 20;	   // System length
+var dl = L / (N-1);
+
+
+var step = 0; //step count
+var skip = 500;
+
+//Particle parameter
+var MASS = 1;
+var RADIUS = 1*dl;
+
+var spring = { k: 1000, l: 0.6*dl };
+
+//particle class
+var Particle = function(parameter){
+	//index
+	this.index = parameter.index;
+	//radius
+	this.radius = parameter.radius;
+	//mass
+	this.mass = parameter.mass;
+	//position vector
+	this.x = parameter.x || 0;
+	this.y = parameter.y || 0;
+	this.z = parameter.z || 0;
+	//velocity vector
+	this.vx = parameter.vx || 0;
+	this.vy = parameter.vy || 0;
+	this.vz = parameter.vz || 0;
+	//force vector
+	this.fx; 
+	this.fy; 
+	this.fz; 
+};
+
+Particle.prototype = {
+	constructor: Particle
+};
+
+//calculation class
+var Calculation = function(p){
+	this.initParticle(p);
+};
+
+Calculation.prototype = {
+	constructor: Calculation,
+	//init Particles
+	initParticle: function(p){
+		for(var i=0; i<p.length; i++){
+			p[i].x = 0;
+			p[i].y = i * dl;
+			p[i].z = 0;
+			this.calculateForce(p);
+		}
+		p[(N/4)].vz = 15;
+		p[3*N/4].vz = -15;
+	},
+	//calculation Force
+	calculateForce: function(p){
+		var force01,force10;
+		var d_r;
+		force01={ x: 0, y: 0, z: 0 };
+		force10={ x: 0, y: 0, z: 0 };
+		d_r={ x: 0, y: 0, z: 0, norm: 0 };
+		for(var i=0; i<N; i++){
+			if(i != 0){
+				force10.x = -force01.x;
+				force10.y = -force01.y;
+				force10.z = -force01.z;
+			}
+			if(i != N-1){
+				d_r.x = p[i+1].x - p[i].x;
+				d_r.y = p[i+1].y - p[i].y;
+				d_r.z = p[i+1].z - p[i].z;
+				d_r.norm = Math.sqrt(d_r.x*d_r.x + d_r.y*d_r.y + d_r.z*d_r.z);
+				force01.x = -spring.k*(spring.l-d_r.norm)*d_r.x/d_r.norm;
+				force01.y = -spring.k*(spring.l-d_r.norm)*d_r.y/d_r.norm;
+				force01.z = -spring.k*(spring.l-d_r.norm)*d_r.z/d_r.norm;
+		}
+			if( i == 0 ){
+				p[i].fx = force01.x;
+				p[i].fy = force01.y;
+				p[i].fz = force01.z;
+			}
+			else if( i == N-1 ){
+				p[i].fx = force10.x;
+				p[i].fy = force10.y;
+				p[i].fz = force10.z;
+			}
+			else{
+				p[i].fx = force01.x+force10.x;
+				p[i].fy = force01.y+force10.y;
+				p[i].fz = force01.z+force10.z;
+			}
+		}
+	},
+	//Velocity Verlet method
+	timeDevelopment: function(p){
+		// conserve f(t)
+		var old_fx = [];
+		var old_fy = [];
+		var old_fz = [];
+
+		for(var i=0; i<p.length; i++){
+			//x(t+dt) = x(t) + v(t)*dt + f(t)*dt*dt/2m
+			p[i].x = p[i].x + p[i].vx*dt + p[i].fx*dt*dt/(2*p[i].mass);
+			p[i].y = p[i].y + p[i].vy*dt + p[i].fy*dt*dt/(2*p[i].mass);
+			p[i].z = p[i].z + p[i].vz*dt + p[i].fz*dt*dt/(2*p[i].mass);
+
+			old_fx[i] = p[i].fx;
+			old_fy[i] = p[i].fy;
+			old_fz[i] = p[i].fz;
+		}
+
+		// f(t+dt)
+		this.calculateForce(p);
+
+		//v(t+dt) = v(t) + (f(t) + f(t+dt))*dt/2m
+		for(var i=0; i<p.length; i++){
+			p[i].vx = p[i].vx + ( old_fx[i] + p[i].fx )*dt/(2*p[i].mass); 
+			p[i].vy = p[i].vy + ( old_fy[i] + p[i].fy )*dt/(2*p[i].mass); 
+			p[i].vz = p[i].vz + ( old_fz[i] + p[i].fz )*dt/(2*p[i].mass); 
+		}
+		p[0].x = 0;
+		p[0].y = 0;
+		p[0].z = 0;
+		p[0].vx = 0;
+		p[0].vy = 0;
+		p[0].vz = 0;
+		
+		p[N-1].x = 0;
+		p[N-1].y = L;
+		p[N-1].z = 0;
+		p[N-1].vx = 0;
+		p[N-1].vy = 0;
+		p[N-1].vz = 0;
+		//p[0] = { vx: 0, vy: 0, vz: 0};
+	}
+};
+
+//particle object
+var p = [];
+
+
+//calculation object
+var cal;
+
+
 //////////////////////////////
 // Window event
 //////////////////////////////
 
 window.addEventListener("load", function (){
-	threeStart(); //three.js start
+		for(var i=0; i<N; i++)
+			p[i] = new Particle({index: i, radius: RADIUS, mass: MASS});
+		cal = new Calculation(p);
+		threeStart(); //three.js start
 });
-	
+
 //////////////////////////////
 //def threeStart()
 //////////////////////////////
@@ -34,7 +195,7 @@ function initThree(){
 	//add canvas to frame
 	canvasFrame.appendChild(renderer.domElement);
 	//set renderer clear color
-	renderer.setClearColor(0x000000, 1.0);
+	renderer.setClearColor(0xE1FCEF, 1.0);
 	//create scene object
 	scene = new THREE.Scene();
 }
@@ -52,11 +213,11 @@ function initCamera(){
 	//create camera object
 	camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 	//camera position
-	camera.position.set(100, 100, 100);
+	camera.position.set(24*1.1, L/2, 0);
 	//camera up vector
 	camera.up.set(0, 0, 1);
 	//camera center vector
-	camera.lookAt({x: 0, y: 0, z: 0});
+	camera.lookAt({x: 0, y: L/2, z: 0});
 }
 
 //////////////////////////////
@@ -64,7 +225,7 @@ function initCamera(){
 //////////////////////////////
 // global var
 var axis; //axis object
-var cube = []; //primitive object
+var sphere = []; //primitive object
 
 function initObject(){
 	//create axis object
@@ -75,44 +236,18 @@ function initObject(){
 	axis.position.set(0, 0, 0);
 
 
-	//create geometry
-	var geometry = new THREE.BoxGeometry(20, 20, 20);
-	//create material
-	var material = new THREE.MeshLambertMaterial({color: 0xFF0000,wireframe: true});
-	//create object
-	cube[0] = new THREE.Mesh(geometry, material);
-	//add object
-	scene.add(cube[0]);
-	//position
-	cube[0].position.set(0, -30, 0);
-
-
-	//create geometry
-	var geometry = new THREE.BoxGeometry(20, 20, 20);
-	//create material
-	var material = new THREE.MeshLambertMaterial({color: 0x00FF00,wireframe: true});
-	//create object
-	cube[1] = new THREE.Mesh(geometry, material);
-	//add object
-	scene.add(cube[1]);
-	//position
-	cube[1].position.set(0, 0, 0);
-
-
-
-	//create geometry
-	var geometry = new THREE.BoxGeometry(20, 20, 20);
-	//create material
-	var material = new THREE.MeshLambertMaterial({color: 0x0000FF, wireframe: true});
-	//create object
-	cube[2] = new THREE.Mesh(geometry, material);
-	//add object
-	scene.add(cube[2]);
-	//position
-	cube[2].position.set(0, 30, 0);
-
-
-
+	for(var i=0; i<N; i++){
+		//create geometry
+		var geometry = new THREE.SphereGeometry(p[i].radius, 20, 20);
+		//create material
+		var material = new THREE.MeshLambertMaterial({color: 0xFF0000 });
+		//create object
+		sphere[i] = new THREE.Mesh(geometry, material);
+		//add object
+		scene.add(sphere[i]);
+		//position
+		sphere[i].position.set(p[i].x, p[i].y, p[i].z);
+	}
 }
 
 //////////////////////////////
@@ -132,7 +267,7 @@ function initLight(){
 	//create object
 	directionalLight1 = new THREE.DirectionalLight(0xFFFFFF, 1.0);
 	//light position
-	directionalLight1.position.set(-50, -50, -100);
+	directionalLight1.position.set(50, 50, 50);
 	//add light
 	scene.add(directionalLight1);
 }
@@ -140,33 +275,20 @@ function initLight(){
 //////////////////////////////
 // def loop()
 //////////////////////////////
-//global var
-var step = 0; //step count
 
 function loop(){
-	//increment of step
-	step++;
 
-	//rotation cube
-	cube[0].rotation.set(step / 100, 0, 0);
-	cube[1].rotation.set(0, step / 100 , 0);
-	cube[2].rotation.set(0, 0 , step / 100);
+	for(var n=0; n<skip; n++){
+		//increment of step
+		step++;
+		cal.timeDevelopment(p);
+	}
+	time = step * dt;
 
-	//rotation light
-	var cx = 100 * Math.cos(step / 50);
-	var cy = 100 * Math.sin(step / 50);
-	//set light
-	directionalLight.position.set(cx, cy, 100);
-
-
-	//rotation camera
-	var cameraX = 100 * Math.cos(step / 100);
-	var cameraY = 100 * Math.sin(step / 100);
-	//set camra 
-	camera.position.set(cameraX, cameraY, 100);
-	camera.up.set(0, 0, 1);
-	camera.lookAt({x: 0, y: 0, z: 0});
-
+	//set draw object
+	for(var i=0; i<N; i++){
+		sphere[i].position.set(p[i].x, p[i].y, p[i].z);
+	}
 
 	//init clear color
 	renderer.clear();
